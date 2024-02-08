e919c00ce9a3045b9d1e3ad3db9e5cb782c611e3
marietcht <marie.tranchant@maif.fr>
2024-02-08 15:05:19 +0100

    again fetch

--- chapter01/exercice04/index.js
+++ chapter01/exercice04/index.js
@@ -7,15 +7,24 @@ export function addWithCallback(a, b, callback) {
 
 export function filterArrayWithCallback(array, filterFunc, callback) {
   setTimeout(() => {
-    callback(filterFunc(array));
+    callback(array.filter(filterFunc));
   }, 1000);
 }
 
 export async function asyncAdd(a, b) {
   setTimeout(() => {
+    let promise = new Promise((resolve, reject) => {
+      var sum = a + b;
+      resolve(sum);
+  }, 1000));
+  promise.then((value) => {
+    asyncAdd(a, b)
+  }).catch((error) => {
+    console.error('error');
+  })
+};
 
-  }, 1000);
-}
+
 
 export async function filterArrayAsync(array, filterFunc) {
   setTimeout(() => {
