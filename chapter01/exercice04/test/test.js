3652f26950be7b1b4de0860a95320c64733e60c8
shiipou <shiishii@nocturlab.fr>
2024-02-08 13:41:59 +0100

    fix: latest test work
    Signed-off-by: shiipou <shiishii@nocturlab.fr>

--- chapter01/exercice04/test/test.js
+++ chapter01/exercice04/test/test.js
@@ -45,12 +45,11 @@ describe('Chapter 1 -> Course 4', _ => {
   describe('Filter an Array with Promise', _ => {
     const filter = (a) => a % 2 == 0;
     [...Array(5).keys()].map(_ => {
-      const inputs = Array(Math.floor(Math.random() * 10)).map(_ => Math.floor(Math.random() * 100))
+      const inputs = [...Array(Math.floor(Math.random() * 10))].map(_ => Math.floor(Math.random() * 100))
       const output = inputs.filter(filter)
-      it(`should return the a filtered array of ${inputs}, it should be ${output}`, async done => {
+      it(`should return the a filtered array of ${inputs}, it should be ${output}`, async () => {
         const value = await filterArrayAsync(inputs, filter)
         assert.equal(JSON.stringify(value), JSON.stringify(output))
-        done()
       })
     })
   })
